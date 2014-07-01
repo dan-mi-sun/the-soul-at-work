b7710cba115e550cc66d7b863af0187b0c5b6514
Daniel Sun <dan.mi.sun@gmail.com>
2014-07-01 18:10:01 +0100

    add failing integration tests for adding languages to profile feature

---
+++ features/step_definitions/edit_languages_steps.rb
@@ -0,0 +1,20 @@
+Given(/^languages exist$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^a user has languages$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the edit profile page has languages$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^they choose to add more than one language$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^their profile should be updated$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
