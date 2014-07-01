6f5af47eff2c485294a9b615190e96e4462d31f6
Daniel Sun <dan.mi.sun@gmail.com>
2014-07-01 17:44:39 +0100

    add failing integration tests for adding skills feature

---
+++ features/skills.feature
@@ -0,0 +1,15 @@
+Feature: Skills
+
+  Background: 
+    Given a user exists
+      And skills exist
+      And a user has skills
+      And a user signs in
+      And the user is on the edit profile page
+      And the edit profile page has skills
+
+  @wip
+  Scenario: Add skills to user profile
+    When they choose to add more than one skills
+      And the user submits the form
+    Then their profile should be updated
---
+++ features/step_definitions/edit_skills_steps.rb
@@ -0,0 +1,20 @@
+Given(/^skills exist$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^a user has skills$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the edit profile page has skills$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^they choose to add more than one skills$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^their profile should be updated$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
