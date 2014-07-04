2750be2f5261a50edaad994ea71f538444f295f3
Daniel Sun <dan.mi.sun@gmail.com>
2014-07-04 12:36:01 +0100

    add failing integration tests for seeing projects on user profile feature

---
+++ features/step_definitions/see_index_of_user_projects_on_profile_steps.rb
@@ -0,0 +1,20 @@
+Given(/^a buyer exists$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^a buyer has projects$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^a buyer signs in$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^that the buyer is on their profile$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^they should see the project displayed on their profile$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
