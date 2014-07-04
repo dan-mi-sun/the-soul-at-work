c52c1f94b88f5683386d17df2efaecd1d258922b
Daniel Sun <dan.mi.sun@gmail.com>
2014-07-04 12:00:48 +0100

    pass tests for projects belong to buyer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140703133048) do
+ActiveRecord::Schema.define(version: 20140704105931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20140703133048) do
     t.date     "closing_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "buyer_id"
   end
 
   create_table "requisite_skills", force: true do |t|
