48c1b1b8cbaaea4de5913646d244d476dc3bb54d
Daniel Sun <dan.mi.sun@gmail.com>
2014-07-07 17:09:17 +0100

    add validations for proposal submission requirements

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,5 +1,9 @@
 class Proposal < ActiveRecord::Base
 
+  validates_length_of :title, :minimum => 4, :allow_blank => false  
+  validates_length_of :description, :minimum => 10, :allow_blank => false 
+  validates_length_of :about, :minimum => 10, :allow_blank => false 
+
   belongs_to :creative
   belongs_to :project
 
