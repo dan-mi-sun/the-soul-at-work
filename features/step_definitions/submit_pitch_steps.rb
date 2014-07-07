fb6f790c831638357058310668dc0d9adc091e1e
Daniel Sun <dan.mi.sun@gmail.com>
2014-07-07 14:00:30 +0100

    add failing integration test for submitting pitch feature

---
+++ features/step_definitions/submit_pitch_steps.rb
@@ -0,0 +1,20 @@
+Given(/^the user is on the project page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^they choose start a pitch$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^then they should be taken to the edit pitch page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^when they complete the form$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^their pitch should be submitted$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
