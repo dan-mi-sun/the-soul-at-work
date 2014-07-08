b7a4c9ee29780ab31fcec01cac29c40b06a433e1
Daniel Sun <dan.mi.sun@gmail.com>
2014-07-08 09:36:33 +0100

    passing integration tests for displaying index of proposals to a project feature

--- features/step_definitions/projects_index_of_proposals_steps.rb
+++ features/step_definitions/projects_index_of_proposals_steps.rb
@@ -1,11 +1,12 @@
 Given(/^a project has proposals$/) do
-  pending # express the regexp above with the code you wish you had
+  @proposal = Proposal.create!(:title => "Ruby Proposal", :description => Faker::Company.bs, :about => Faker::Commerce.department)
+  @project.proposals << @proposal
 end
 
 Given(/^We are on the project show page$/) do
-  pending # express the regexp above with the code you wish you had
+  visit project_path(@project)
 end
 
 Then(/^they should see the proposals which have been submitted to the project$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(page.has_content?(@proposal.title)).to be true
 end
