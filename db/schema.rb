d04eb389ee31f8ee00126da6a51316abd03922d6
Daniel Sun <dan.mi.sun@gmail.com>
2014-07-10 19:51:11 +0100

    add first name, last name, address to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140709140414) do
+ActiveRecord::Schema.define(version: 20140710185000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,11 +101,10 @@ ActiveRecord::Schema.define(version: 20140709140414) do
     t.string   "location"
     t.text     "about"
     t.string   "avatar"
-    t.string   "address"
-    t.string   "balanced_customer_uri"
+    t.string   "balanced_card_uri"
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "balanced_card_uri"
+    t.string   "address"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
