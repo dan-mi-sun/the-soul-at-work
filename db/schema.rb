96817093f0da0c52383f4b6f6c4944b7f35bc186
Daniel Sun <dan.mi.sun@gmail.com>
2014-07-10 20:19:38 +0100

    add balanced customer uri to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140710185000) do
+ActiveRecord::Schema.define(version: 20140710191149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,7 @@ ActiveRecord::Schema.define(version: 20140710185000) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "address"
+    t.string   "balanced_customer_uri"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
